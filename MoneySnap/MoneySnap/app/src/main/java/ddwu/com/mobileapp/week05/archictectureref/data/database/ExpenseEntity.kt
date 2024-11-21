4be1894952bc28ec36ef270cf41ece43c0d59bb0
sondahyun <sonda2356@naver.com>
2024-11-21 18:34:06 +0900

    init

--- MoneySnap/MoneySnap/app/src/main/java/ddwu/com/mobileapp/week05/archictectureref/data/database/ExpenseEntity.kt
+++ MoneySnap/MoneySnap/app/src/main/java/ddwu/com/mobileapp/week05/archictectureref/data/database/ExpenseEntity.kt
@@ -1,8 +1,11 @@
 package ddwu.com.mobileapp.week05.archictectureref.data.database
 
-@Entity (tableName = "ref_table")
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "ref_table")
 data class ExpenseEntity(
-    @PrimaryKey (autoGenerate = true)
+    @PrimaryKey(autoGenerate = true)
     val _id: Int,
     val name: String
 )
