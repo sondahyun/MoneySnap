21f550df8dabd8a50befc35e9daa3f4af258f17e
gahyunkim <mapledt001@naver.com>
2024-12-25 00:21:05 +0900

    feat: 화면 띄우기

--- app/src/main/java/com/example/moneysnap/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/moneysnap/ui/home/HomeFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.moneysnap.R
+import com.example.moneysnap.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 }
